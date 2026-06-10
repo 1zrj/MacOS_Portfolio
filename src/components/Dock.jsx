7c62eb9b7e06dbf2f141d33da23aa2f48f7050b9
zrj <819519226@qq.com>
2026-06-10 11:54:17 +0800

    implement a hoc for managing window states and add the first window - terminal

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { useRef } from "react";
 import { dockApps } from "#constants";
 import { Tooltip } from "react-tooltip";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "#store/window";
 
 const Dock = () => {
+  const { openWindow, closeWindow, windows } = useWindowStore();
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -58,6 +59,25 @@ const Dock = () => {
       dock.removeEventListener("mouseleave", resetIcons);
     };
   }, []);
+
+  const toggleApp = (app) => {
+    if (!app.canOpen) return;
+
+    const window = windows[app.id];
+
+    if (!window) {
+      console.log(`Window not found for app: ${app.id}`);
+      return;
+    }
+
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
+
+    // console.log(windows);
+  };
 
   return (
     <section id="dock">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { navLinks, navIcons } from "#constants";
 import dayjs from "dayjs";
 
